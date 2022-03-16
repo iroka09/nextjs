cca118c549db4551ef19b5b636c0566a8ddc7a3b
iroka09 <irokatochukwu@gmail.com>
2022-03-16 08:54:59 +0100

    sf

--- pages/api/test.js
+++
@@ -1,5 +0,0 @@
-//every file stored inside page/api will be loaded as server i.e like an expressjs but not express, but will be exported as react i.e "export default" instead of module.exports
-
-export default function(req,res){
-  res.send({done: true})
-}
