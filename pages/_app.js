c1f2a9758d137b31f3deda6b5709c1d0e5f6e097
iroka09 <irokatochukwu@gmail.com>
2022-04-13 02:57:25 +0100

    ll

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-
-import React, {useState, useEffect} from "react";
+import React, {
+  useState,
+  useEffect
+} from "react";
 import Head from "next/head"
 import Container from "@mui/material/Container"
 import CssBaseline from "@mui/material/CssBaseline"
@@ -29,9 +31,17 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import MenuIcon from '@mui/icons-material/Menu';
 import TreeItem from '@mui/lab/TreeItem';
-import {ThemeProvider, createTheme} from "@mui/material/styles"
-import {Provider} from "react-redux"
-import {CookiesProvider, useCookies} from "react-cookie"
+import {
+  ThemeProvider,
+  createTheme
+} from "@mui/material/styles"
+import {
+  Provider
+} from "react-redux"
+import {
+  CookiesProvider,
+  useCookies
+} from "react-cookie"
 import reduxStore from "../components/redux/store"
 import "../styles/global_style.css";
 
@@ -41,115 +51,130 @@ const cookieOptions = {
 
 
 
-function App({Component, pageProps}){
-  
-  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
-  
-  const [themeCode, setThemeCode] = React.useState(pageProps.cookies?.themeCode);
-  
-  const [appliedTheme, setAppliedTheme] = React.useState(themeCode?.split("_index=")[0])
-  
-  const [selectedThemeCodeIndex, setSelectedThemeCodeIndex] = React.useState(+(themeCode?.split("_index=")[1] || Infinity));
-  
-  const [isCookieDrawerOpen, setIsCookieDrawerOpen] = React.useState(false);
-  
-  const [acceptedCookiePolicy, setAcceptedCookiePolicy] = React.useState(pageProps.cookies?.acceptedCookiePolicy);
-  
-  const [isDarkMode, setDarkMode] = useState(pageProps.cookies?.isDarkMode);
-  
-  const [cookies, setCookie, removeCookie] = useCookies([]) //dependency array of cookie names that should re-render this component when changed. e.g ["isDarkMode", "themeCode"]
+function App( {
+  Component, pageProps
+}) {
 
-  const [anchorEl, setAnchorEl] = React.useState();
-  
+  const [isDrawerOpen,
+    setIsDrawerOpen] = React.useState(false);
+
+  const [themeCode,
+    setThemeCode] = React.useState(pageProps.cookies?.themeCode);
+
+  const [appliedTheme,
+    setAppliedTheme] = React.useState(themeCode?.split("_index=")[0])
+
+  const [selectedThemeCodeIndex,
+    setSelectedThemeCodeIndex] = React.useState(+(themeCode?.split("_index=")[1] || Infinity));
+
+  const [isCookieDrawerOpen,
+    setIsCookieDrawerOpen] = React.useState(false);
+
+  const [acceptedCookiePolicy,
+    setAcceptedCookiePolicy] = React.useState(pageProps.cookies?.acceptedCookiePolicy);
+
+  const [isDarkMode,
+    setDarkMode] = useState(pageProps.cookies?.isDarkMode);
+
+  const [cookies,
+    setCookie,
+    removeCookie] = useCookies([]) //dependency array of cookie names that should re-render this component when changed. e.g ["isDarkMode", "themeCode"]
+
+  const [anchorEl,
+    setAnchorEl] = React.useState();
+
   const ele = React.useRef();
-  
+
   const isThemeCodesMenuOpen = Boolean(anchorEl);
-  
+
   const handleThemeCodesMenu = () => {
     setAnchorEl(ele.current);
   };
-  
-  const handleAppliedTheme = ()=>{
+
+  const handleAppliedTheme = ()=> {
     setAppliedTheme(themeCode?.split("_index=")[0]);
     setCookie("themeCode", themeCode, cookieOptions);
   }
-  
-  const handleCloseThemeCodesMenu = (byButton,hex,i) => {
-    if(byButton===true) {
+
+  const handleCloseThemeCodesMenu = (byButton, hex, i) => {
+    if (byButton === true) {
       setSelectedThemeCodeIndex(i)
       setThemeCode(hex+"_index="+i)
     }
     setAnchorEl(null);
   };
-  
-  const handleAcceptCookiePolicy = ()=>{
+
+  const handleAcceptCookiePolicy = ()=> {
     setAcceptedCookiePolicy(true);
-    setCookie("acceptedCookiePolicy", "yes" , cookieOptions);
+    setCookie("acceptedCookiePolicy", "yes", cookieOptions);
   }
 
-const list = React.useMemo(()=>{
-let myList = []
-for(let color in colors){
-  for(let x = 100; x<1000; ){
-    let hex = colors[color][x];
-    if(hex?.trim()){
-      myList.push({
-        colorName: color,
-        colorCode: hex,
-      })
+  const list = React.useMemo(()=> {
+    let myList = []
+    for (let color in colors) {
+      for (let x = 100; x < 1000;) {
+        let hex = colors[color][x];
+        if (hex?.trim()) {
+          myList.push({
+            colorName: color,
+            colorCode: hex,
+          })
+        }
+        x += 100;
+      }
     }
-    x += 100;
-  }
-}
-return myList
-}, []);
+    return myList
+  },
+    []);
 
-const theme = React.useMemo(()=>(
-createTheme({
-  palette: {
-    mode: isDarkMode? "dark" : "light",
-    ...(appliedTheme? 
-      {
-        primary: {
-          main: appliedTheme
+  const theme = React.useMemo(()=>(
+    createTheme({
+      palette: {
+        mode: isDarkMode? "dark": "light",
+        ...(appliedTheme?
+          {
+            primary: {
+              main: appliedTheme
+            }
+          }: {})
+      },
+      typography: {
+        fontFamily: '"Dosis", "Smooch Sans", "Roboto", "Helvetica", "Arial", sans-serif',
+        button: {
+          textTransform: "initial"
         }
-      } : {})
-  },
-  typography: {
-    fontFamily: '"Dosis", "Smooch Sans", "Roboto", "Helvetica", "Arial", sans-serif',
-    button: {
-      textTransform: "initial"
-    }
-  },
-  components: {
-    MuiPaper: {
-      styleOverrides: {
-        root:{
-          border: "none"
+      },
+      components: {
+        MuiPaper: {
+          styleOverrides: {
+            root: {
+              border: "none"
+            }
+          }
+        },
+        MuiButton: {
+          defaultProps: {
+            disableRipple: true,
+            //variant: "outlined"
+          }
         }
       }
-    },
-    MuiButton: {
-      defaultProps: {
-        disableRipple: true,
-        variant: "outlined"
-      }
-    }
-  }
-})
-), [isDarkMode, appliedTheme]);
-    
-  const disableScroll = ()=>{
+    })
+  ),
+    [isDarkMode,
+      appliedTheme]);
+
+  const disableScroll = ()=> {
     document.body.style.overflow = "hidden"
   }
-  
-  const enableScroll = ()=>{
+
+  const enableScroll = ()=> {
     document.body.style.overflow = "initial"
   }
-  
+
   //accept cookie policy prompt
-  React.useEffect(()=>{
-    if(!acceptedCookiePolicy) {
+  React.useEffect(()=> {
+    if (!acceptedCookiePolicy) {
       let fn = setTimeout(function() {
         setIsCookieDrawerOpen(true);
         disableScroll();
@@ -159,155 +184,159 @@ createTheme({
   })
 
   return (
-  <ThemeProvider theme={theme}>    
-  <Provider store={reduxStore}>    
-  <CookiesProvider>    
+    <ThemeProvider theme={theme}>
+  <Provider store={reduxStore}>
+  <CookiesProvider>
     <Head>
       <title>{pageProps.title}</title>
     </Head>
-    
     <CssBaseline />
 
-  <AppBar sx={{
-    px: 1, 
-    py: 0.6
-  }}>
-    <div style={{display:"flex",alignItems:"center"}}>
-      <IconButton sx={{color: "primary.contrastText"}} onClick={()=>setIsDrawerOpen(true)}>
+  <AppBar sx={ {
+      px: 1,
+      py: 0.6
+    }}>
+    <div style={ { display: "flex",
+        alignItems: "center" }}>
+      <IconButton sx={ { color: "primary.contrastText" }} onClick={()=>setIsDrawerOpen(true)}>
         <MenuIcon />
       </IconButton>
-      <div style={{
-        padding: "0 20px",
-        borderRadius: 4,
-        maxWidth: "70%",
-        backgroundColor: "rgba(225,225,225,0.14)",
-        marginLeft:2,
-        display:"flex",
-        alignItems:"center",
-        justifyContent:"center",
-      }}>
-        <InputBase placeholder="Search word..." sx={{my:"2px", color: "primary.contrastText"}}/>
+      <div style={ {
+          padding: "0 20px",
+          borderRadius: 4,
+          maxWidth: "70%",
+          backgroundColor: "rgba(225,225,225,0.14)",
+          marginLeft: 2,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}>
+        <InputBase placeholder="Search word..." sx={ { my: "2px",
+            color: "primary.contrastText" }} />
       </div>
- 
-        <Divider sx={{m: "0 3px 0 auto", backgroundColor: theme.palette.background.paper,height:22}} orientation="vertical"/>
-      
-      <IconButton 
-        sx={{color: "primary.contrastText"}} 
-        onClick={()=>{
-          if(isDarkMode){
+        <Divider sx={ { m: "0 3px 0 auto",
+          backgroundColor: theme.palette.background.paper,
+          height: 22 }} orientation="vertical" />
+      <IconButton
+        sx={ { color: "primary.contrastText" }}
+        onClick={()=> {
+          if (isDarkMode) {
             setDarkMode(false)
             removeCookie("isDarkMode")
-          }
-          else {
+          } else {
             setDarkMode(true)
             setCookie("isDarkMode", "yes", cookieOptions)
           }
-      }}>
-        {(isDarkMode)? <LightModeIcon/> : <DarkModeIcon/>}
+        }}>
+        {(isDarkMode)? <LightModeIcon />: <DarkModeIcon />}
       </IconButton>
     </div>
   </AppBar>
-  
-<Container>
+      <Container>
 
-  <div style={{height:60}}></div>
+  <div style={ { height: 60 }}></div>
 
-  <noscript style={{display:"block", margin: "5px auto"}}>
-    <Alert 
-      variant="outlined" 
-      severity="error"
-      >
+  <noscript style={ { display: "block",
+        margin: "5px auto" }}>
+    <Alert
+        variant="outlined"
+        severity="error"
+        >
       Your browser doesn't support Javascript, some functionalities won't work, please upgrade your browser.
     </Alert>
   </noscript>
-  
-  <div style={{
-    margin:"10px 0 30px", 
-    display:"flex", 
-    alignItems:"center",
-    flexDirection:"column"
-  }}>
-  <code style={{color:themeCode?.split("_index=")[0]||"black",marginBottom:10}}>
-  {(themeCode?.split("_index=")[0]||"Please select theme")?.toUpperCase()}
+  <div style={ {
+        margin: "10px 0 30px",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column"
+      }}>
+  <code style={ { color: themeCode?.split("_index=")[0] || "black",
+          marginBottom: 10 }}>
+  {(themeCode?.split("_index=")[0] || "Please select theme")?.toUpperCase()}
   </code>
   <ButtonGroup
-    variant="text"
-  >
-    <Button 
-      sx={{flex: 1}} 
-      ref={ele}
-      onClick={handleAppliedTheme}
-      >
+        variant="text"
+        >
+    <Button
+          sx={ { flex: 1 }}
+          ref={ele}
+          onClick={handleAppliedTheme}
+          >
       Apply
     </Button>
-    <Button size="small" sx={{px:"1px"}} onClick={handleThemeCodesMenu}>
-       {(isThemeCodesMenuOpen)? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon />}
+    <Button size="small" sx={ { px: "1px" }} onClick={handleThemeCodesMenu}>
+       {(isThemeCodesMenuOpen)? <KeyboardArrowUpIcon />: <KeyboardArrowDownIcon />}
     </Button>
     </ButtonGroup>
     <Menu
         anchorEl={anchorEl}
         open={isThemeCodesMenuOpen}
         onClose={handleCloseThemeCodesMenu}
-        anchorOrigin={{
+        anchorOrigin={ {
           vertical: 'bottom',
           horizontal: 'center',
         }}
-        transformOrigin={{
+        transformOrigin={ {
           vertical: 'top',
           horizontal: 'center',
         }}
-      transitionComponent={Zoom} 
-      >
+        transitionComponent={Zoom}
+        >
       {
         list.map((_color, i)=>(
-          <MenuItem 
-              key={i}
-              selected={selectedThemeCodeIndex===i}
-              onClick={()=>handleCloseThemeCodesMenu(true,_color.colorCode, i)}
-              sx={{color:_color.colorCode}}
-          >
+          <MenuItem
+            key={i}
+            selected={selectedThemeCodeIndex === i}
+            onClick={()=>handleCloseThemeCodesMenu(true, _color.colorCode, i)}
+            sx={ { color: _color.colorCode }}
+            >
           {_color.colorName+" | "+_color.colorCode}
           </MenuItem>
         ))
-      }
+        }
       </Menu>
-    <p>After selecting theme from the theme menu, press on the APPLY button to apply.</p>
+    <p>
+After selecting theme from the theme menu, press on the APPLY button to apply.
+      </p>
     </div>
-  
   <Component {...pageProps} />
-  
   </Container>
-  
-  <footer style={{backgroundColor: theme.palette.divider, padding: "5px 2px"}}>
-  
+  <footer style={ { backgroundColor: theme.palette.divider,
+      padding: "5px 2px" }}>
     <Typography color="primary">
       Customer Care
     </Typography>
     <Typography>
       +234 813 077 0603
     </Typography>
-  </footer>
+    </footer>
 
 
-<SwipeableDrawer
-  anchor="left"
-  open={isDrawerOpen}
-  onClose={()=>{
-    setIsDrawerOpen(false);
-    enableScroll();
-  }}
-  onOpen={()=>{
-    setIsDrawerOpen(true);
-    disableScroll()
-  }}
->
+      <SwipeableDrawer
+      anchor="left"
+      open={isDrawerOpen}
+      onClose={()=> {
+        setIsDrawerOpen(false);
+        enableScroll();
+      }}
+      onOpen={()=> {
+        setIsDrawerOpen(true);
+        disableScroll()
+      }}
+      >
     <TreeView
-      aria-label="multi-select"
-      defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpandIcon={<ChevronRightIcon />}
-      multiSelect
-      sx={{ height: 216, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
-    >
+        aria-label="multi-select"
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+        multiSelect
+        sx={ {
+          height: 216,
+          flexGrow: 1,
+          maxWidth: 400,
+          overflowY: 'auto'
+        }}
+        >
       <TreeItem nodeId="1" label="Applications">
         <TreeItem nodeId="2" label="Calendar" />
         <TreeItem nodeId="3" label="Chrome" />
@@ -322,47 +351,51 @@ createTheme({
         </TreeItem>
       </TreeItem>
     </TreeView>
-</SwipeableDrawer>
+      </SwipeableDrawer>
 
-<Drawer
-  anchor="bottom"
-  open={isCookieDrawerOpen}
-  onClose={()=>{
-    setIsCookieDrawerOpen(false);
-    enableScroll()
-  }}
-  onOpen={()=>{
-    setIsCookieDrawerOpen(true);
-    disableScroll()
-  }}
->
-<div style={{padding: "3px"}}>
-<center style={{padding:"10px 0", fontWeight:400}}>Cookie Policy</center>
-<Divider />
-  <div style={{margin:"30px auto",width: "90%"}}>
+      <Drawer
+      anchor="bottom"
+      open={isCookieDrawerOpen}
+      onClose={()=> {
+        setIsCookieDrawerOpen(false);
+        enableScroll()
+      }}
+      onOpen={()=> {
+        setIsCookieDrawerOpen(true);
+        disableScroll()
+      }}
+      >
+        <div style={ { padding: "3px" }}>
+          <center style={ { padding: "10px 0",
+        fontWeight: 400 }}>Cookie Policy</center>
+          <Divider />
+  <div style={ { margin: "30px auto",
+        width: "90%" }}>
    We use Cookies to store our users' choice.
-  </div>
-  <div style={{display:"flex",justifyContent:"space-around"}} onClick={()=>setIsCookieDrawerOpen(false)}>
-    <Button 
-      sx={{color:"#aaa"}}
-      fullWidth
-      sx={{borderRadius:0,color:"#777"}}
-      >Cancel</Button>
+      </div>
+  <div style={ { display: "flex",
+          justifyContent: "space-around" }} onClick={()=>setIsCookieDrawerOpen(false)}>
+    <Button
+          sx={ { color: "#aaa" }}
+          fullWidth
+          sx={ { borderRadius: 0,
+            color: "#777" }}
+          >Cancel</Button>
     <div>
     <Divider orientation="vertical" />
-    </div>
-    <Button 
-      onClick={handleAcceptCookiePolicy}
-      fullWidth
-      sx={{borderRadius:0}}
-    >Accept</Button>
-  </div>
-  </div>
-</Drawer>
+        </div>
+    <Button
+          onClick={handleAcceptCookiePolicy}
+          fullWidth
+          sx={ { borderRadius: 0 }}
+          >Accept</Button>
+      </div>
+      </div>
+      </Drawer>
 
-</CookiesProvider>
-</Provider>
-</ThemeProvider>
-)}
+    </CookiesProvider>
+    </Provider>
+    </ThemeProvider>
+  )}
 
 export default App
