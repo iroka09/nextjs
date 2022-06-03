ae6f013fccbedfaf75f1aeee79e56dfedd49f5bd
iroka09 <irokatochukwu@gmail.com>
2022-06-03 05:35:15 +0100

    l

--- .profile
+++
@@ -1,3 +0,0 @@
-export NVM_DIR=/data/data/com.termux/files/home/.nvm
-[ -s /nvm.sh ] && \. /nvm.sh  
-# This loads nvm
--- next.json.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({
-  swcMinify: true, // it should be false by default 
-  env: {
-    developer: "Iroka Ntomchukwu"
-  },
-  images: {
-    domains: ["https://picsum.photos"],
-  },
-})
