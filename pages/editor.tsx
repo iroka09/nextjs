d9c10949dc9389d3a21f5292ebf1c608b332c247
iroka09 <irokatochukwu@gmail.com>
2022-06-06 23:57:08 +0100

    l

---
+++ pages/editor.tsx
@@ -0,0 +1,72 @@
+
+import React, {useState, useEffect, memo} from "react"
+import Head from "next/head"
+import dynamic from "next/dynamic"
+import Button from "@mui/material/Button"
+import { EditorState } from 'draft-js';
+
+const Editor =  dynamic<EditorProps>(()=> import('react-draft-wysiwyg').then(x=>x.Editor), {ssr: false})
+
+function App(){
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  
+  const [loadedEditor, setLoadedEditor] = useState(false)
+
+  const handleEditorState = (text) => {
+    setEditorState(text);
+  }
+
+  useEffect(()=>{
+   // setLoadedEditor(true)
+  }, [])
+
+  return (
+    <div>
+    
+      <Head>
+        <meta charSet="utf-8" />
+        <title>Editor Draft</title>
+      </Head>
+      
+    {loadedEditor &&
+      <Editor
+        initialEditorState={editorState}
+        wrapperClassName="demo-wrapper"
+        editorClassName="demo-editor"
+        toolbarClassName="demo-toolbar"
+        onEditorStateChange={handleEditorState}
+      />
+    }
+    <Button
+      variant="outlined"
+      onClick={()=>setLoadedEditor(true)}
+      sx={{my:7}}
+    >
+      SHOW EDITOR
+    </Button>
+    </div>
+  )
+}
+
+export default memo(App)
+
+export function getServerSideProps({req}){
+  return({
+    props: {
+      cookies: req.cookies
+    }
+  })
+}
+
+/*
+{loadedEditor &&
+      <Editor
+        initialContentState={contentState}
+        wrapperClassName="demo-wrapper"
+        editorClassName="demo-editor"
+        toolbarClassName="demo-toolbar"
+        onContentStateChange={onContentStateChange}
+      />
+      }
+
+*/
