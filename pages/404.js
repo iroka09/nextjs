ae4ed84ca1933cd82ada8d8b7ec7e505a236208f
iroka09 <irokatochukwu@gmail.com>
2022-06-06 15:03:25 +0100

    l

--- pages/404.js
+++ pages/404.js
@@ -4,16 +4,10 @@ import Typography from "@mui/material/Typography"
 import Stack from "@mui/material/Stack"
 
 export default function App() {
-  const done= React.useState("click me");
-    
-    const handle = ()=>{
-      done[1](Math.random())
-    }
   
   return (
     <>
-    <h3 onClick={handle}>{done[0]}</h3>
-    <Stack direction="row" sx={ { my: 20 }} spacing={3} alignItems="center">
+    <Stack direction="row" sx={ { my: 20, mx:3 }} spacing={3} alignItems="center">
     <Typography
       color="error"
       variant="h5"
