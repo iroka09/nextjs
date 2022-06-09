0bddcb462caa1dea79c9e2dbe1c2a9c0890d2304
iroka09 <irokatochukwu@gmail.com>
2022-06-09 02:45:08 +0100

    l

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -3,11 +3,26 @@ import React, {useState, useEffect, memo} from "react"
 import Head from "next/head"
 import dynamic from "next/dynamic"
 import Button from "@mui/material/Button"
+import CircularProgress from "@mui/material/CircularProgress"
 import {useTheme} from "@mui/material/styles"
 import { EditorState } from 'draft-js';
 import { EditorProps } from 'react-draft-wysiwyg';
 
-const Editor =  dynamic<EditorProps>(()=> import('react-draft-wysiwyg').then(x=>x.Editor), {ssr: false})
+const Editor =  dynamic <EditorProps> (()=> import('react-draft-wysiwyg')
+  .then(x=>x.Editor), {
+  ssr: false,
+  loading: (
+    <div style={{
+      height: 300,
+      width: "100%",
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+    }}>
+      <CircularProgress />
+    </div>
+  )
+})
 
 function App(){
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
