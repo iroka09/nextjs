6510ea4d9393314968c92e8b3093402d88ac39ac
iroka09 <irokatochukwu@gmail.com>
2022-06-16 21:43:32 +0100

    l

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,10 +1,9 @@
 
 export default async function handler(req, res) {
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== 7070) {
-    return res.status(401).json({ message: 'Invalid token' })
+  if (req.query.secret !== "7070") {
+    return res.status(401).json({ message: 'Invalid token', secret: req.query.secret, type: typeof req.query.secret})
   }
-
   try {
     await res.unstable_revalidate('/path-to-revalidate')
     return res.json({ revalidated: true })
