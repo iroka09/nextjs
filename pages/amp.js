9f8b7971f955c2b54ea800364630a76014b88f4b
iroka09 <irokatochukwu@gmail.com>
2022-06-28 13:40:27 +0100

    l

---
+++ pages/amp.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {useAmp} from "next/amp";
+
+export const config = {amp: "hybrid"};
+
+export default function App(){
+  const isAmp = useAmp()
+  return(<>
+    {
+      (isAmp)? (
+      <>
+        <h3>Amp page</h3>
+        <amp-img width="300" height="300" src="/pic1.jpg" layout="fixed" />
+      </>
+      ) : (
+      <>
+        <h3>Normal page</h3>
+        <img width="300" height="300" src="/pic2.jpg" />
+      </>
+      )
+    }
+  </>)
+}
