8fe7a4094f7ea78d5a3a61abbd72babebbb60b4e
iroka09 <irokatochukwu@gmail.com>
2022-06-30 03:27:32 +0100

    l

--- components/Swipe.js
+++ components/Swipe.js
@@ -9,31 +9,65 @@ import {autoPlay, virtualize, bindKeyboard} from "react-swipeable-views-utils"
 const Swiper = bindKeyboard(autoPlay(SwipeableViews));
 
 export default function App(props){
-  const [index, setIndex] = useState(0);
-  const handleChangeIndex = (i)=>{
-    setIndex(i)
+  const [index1, setIndex1] = useState(0);
+  const [index2, setIndex2] = useState(0);
+  
+  const handleChangeIndex1 = (i)=>{
+    setIndex1(i)
+  }
+  const handleChangeIndex2 = (i)=>{
+    setIndex2(i)
   }
   return ( <>
-    <h1> {index} </h1>
+    <h1> {index1} </h1>
     <Swiper
       resistance
-      onChangeIndex={handleChangeIndex}
+      onChangeIndex={handleChangeIndex1}
       slideStyle={{
-        boxShadow: "0 2px 1px -1px black",
+        boxShadow: "0 3px 1px -1px black",
       }}
       >
       {Array.from(Array(5)).map((x, i)=>(
       <Box
         sx={{
           flexGrow: 1,
-          height: 200,
+          height: 250,
           position: "relative",
         }}
       >
         <Image
           key={i} 
-          src="http://picsum.photos/?random"
-          loader: {({src})=> src}
+          src="https://picsum.photos/400/400/?random"
+          loader: {(obj)=> obj.src}
+          layout='fill'
+          objectFit= "cover"
+          placeholder="blur"
+          quality={100}
+        />
+      </Box>
+      ))}
+    </Swiper>
+    
+    <h1> {index2} </h1>
+    <Swiper
+      resistance
+      onChangeIndex={handleChangeIndex2}
+      slideStyle={{
+        boxShadow: "0 3px 1px -1px black",
+      }}
+      >
+      {Array.from(Array(5)).map((x, i)=>(
+      <Box
+        sx={{
+          flexGrow: 1,
+          height: 250,
+          position: "relative",
+        }}
+      >
+        <img
+          key={i} 
+          src="https://picsum.photos/400/400/?random"
+          loader: {(obj)=> obj.src}
           layout='fill'
           objectFit= "cover"
           placeholder="blur"
