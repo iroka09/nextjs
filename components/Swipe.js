e96e71534035459625c3bbdc8907af99c5d90332
iroka09 <irokatochukwu@gmail.com>
2022-07-01 12:43:02 +0100

    l

--- components/Swipe.js
+++ components/Swipe.js
@@ -105,7 +105,7 @@ export default function App(props){
       <IconButton
         onClick={handleNext}
       >
-        <ChevronRighttIcon />
+        <ChevronRightIcon />
       </IconButton>
     <div 
       style={{
