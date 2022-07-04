d5606dfc7eb2ae43bc50be3ae853d52acf8a599b
iroka09 <irokatochukwu@gmail.com>
2022-07-04 11:04:30 +0100

    l

--- components/Spinners.js
+++ components/Spinners.js
@@ -2,19 +2,21 @@
 import React, {useEffect, useMemo, memo} from "react"
 import random from "random"
 import {useTheme} from "@mui/material/styles"
-import * as spinners from "react-spinners";
+// import * as spinners from "react-spinners";
+import {RingLoader} from "react-spinners";
 
 function App(props){
   
   const theme = useTheme();
   
   const  MyLoader = useMemo(()=>{
-    let arr = [];
+   /*let arr = [];
     for(let x in spinners) {
       arr.push(spinners[x])
     }
     let length = arr.length - 0;
-    return arr[random.int(0, length)];
+    return arr[random.int(0, length)];*/
+    return RingLoader;
   }, [])
   
   return (
