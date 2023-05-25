021b797175ec57be6a46e873564e71709bb4c994
iroka09 <irokatochukwu@gmail.com>
2023-05-25 06:22:45 +0100

    l

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import React from "react"
 import "../styles/global_style.css";
 import "../styles/tailwind-styles.css";
-import "bootstrap-icons/font/bootstrap-icons.min.css";
 import 'react-alice-carousel/lib/alice-carousel.css';
+//import "bootstrap-icons/font/bootstrap-icons.min.css";
  
 export default function(props){
   const {Component, pageProps} = props;
