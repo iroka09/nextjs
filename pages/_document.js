56c54c67836d2b2a3b2eb3ed74401f395c71f79e
iroka09 <irokatochukwu@gmail.com>
2023-05-29 06:55:17 +0100

    l

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,8 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default ()=>(
     <Html>
       <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dosis&display=swap"/>
         <link rel="icon" href="/favicon.ico" sizes="50x50"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dosis&display=swap"/>
       </Head>
       <body>
         <Main />
